59781fba110121f40abe981424ae6f402266c66f
m0t0k1ch1 <m0t0k1ch1.310@gmail.com>
2013-11-18 16:31:22 +0900

    update build.scala

--- project/build.scala
+++ project/build.scala
@@ -29,15 +29,14 @@ object IsuconBuild extends Build {
         "org.eclipse.jetty" % "jetty-webapp" % "8.1.8.v20121106" % "container",
         "org.eclipse.jetty.orbit" % "javax.servlet" % "3.0.0.v201112011016" % "container;provided;test" artifacts (Artifact("javax.servlet", "jar", "jar")),
 
-        "com.typesafe.slick" %% "slick" % "1.0.1",
         "c3p0" % "c3p0" % "0.9.1.2",
+        "com.typesafe.slick" %% "slick" % "1.0.1",
         "mysql" % "mysql-connector-java" % "5.1.13",
 
-        "org.scalatra" %% "scalatra-json" % "2.2.1",
-        "org.json4s"   %% "json4s-jackson" % "3.2.4",
-
+        "commons-codec" % "commons-codec" % "1.6",
         "org.apache.directory.studio" % "org.apache.commons.io" % "2.4",
-        "commons-codec" % "commons-codec" % "1.6"
+        "org.json4s"   %% "json4s-jackson" % "3.2.4",
+        "org.scalatra" %% "scalatra-json" % "2.2.1"
       ),
       scalateTemplateConfig in Compile <<= (sourceDirectory in Compile) { base =>
         Seq(
